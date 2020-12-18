cd62aca0510c09d157f72338c52d6d2de0bd584e
Alyn <rosualinclaudiu@yahoo.com>
2020-12-18 20:41:04 +0200

    1

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
-setTimeout(welcome, 3000)
+var time = 3000
+time = time + 1000
+var message = "Bine ai venit!"
+var isLate = true
+
+setTimeout(welcome, time)
 function welcome() {
     alert("Bine ai venit!")
     alert("Welcome!") 
 }
 
+// var message "Hello"
+// var name "Alin"
+// alert(message+name)
