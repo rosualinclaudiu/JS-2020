ca809bc1c06151168dc03bad7eb9a86144f67f74
Alyn <rosualinclaudiu@yahoo.com>
2020-12-21 20:52:55 +0200

    1

--- hours.js
+++ hours.js
@@ -1,5 +1,15 @@
-var hour = 0
-while (hour < 24) {
+//var hour = 0
+// while (hour < 24) {
+//     document.writeln(hour) // html le afiseaza una langa
+//     document.writeln("<br>") // pentru a spune catre html sa le scrie una sub alta
+// }
+//    sau mai jos
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+for (var hour = 0; hour < 24; hour++) {
     document.writeln(hour)
-    hour++
+    document.writeln("<br>")
 }
