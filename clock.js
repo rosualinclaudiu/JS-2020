3ccbb0840f63dcce50494f5449d48239d56c5205
Alyn <rosualinclaudiu@yahoo.com>
2020-12-21 20:22:51 +0200

    1

--- clock.js
+++ clock.js
@@ -1,11 +1,31 @@
 var hour = new Date().getHours()
+hour = 10  //doar ca sa putem modifica ora sistemului
 
-if (hour<12) {
-    document.write("Buna dimineata!")
-} else if (hour > 12 && hour < 18) {
-    document.write("Buna ziua!")
-} else if (hour > 18 && hour < 24) {
-    document.write("Buna seara!")
-} else {
+// varianta 1
+
+// if (hour < 12 && hour > 1) {
+//     document.write("Buna dimineata!")
+// } else if (hour >= 12 && hour < 18) {
+//     document.write("Buna ziua!")
+// } else if (hour >= 18 && hour <= 23) {
+//     document.write("Buna seara!")
+// } else {
+//     document.write("Somn usor!")
+// }
+
+if (hour < 2) {
     document.write("Somn usor!")
+} else if (hour < 12) {
+    document.write("Buna dimineata!")
+} else if (hour < 18) {
+    document.write("Buna ziua!")
+} else {
+    document.write("Buna seara!")
 }
+
+year = document.getElementById('year').value // pentru exercitiu cu generatii pe ani
+
+if (hour = 19) {
+    document.write("Bine ai venit la curs!")
+}
+
---
+++ hours.js
@@ -0,0 +1,5 @@
+var hour = 0
+while (hour < 24) {
+    document.writeln(hour)
+    hour++
+}
