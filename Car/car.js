8b5d44aaf02b8849f337cf4f370b2f45f0fe8574
Alyn <rosualinclaudiu@yahoo.com>
2021-01-08 20:51:40 +0200

    1

--- Car/car.js
+++ Car/car.js
@@ -5,7 +5,25 @@ let dacia = {
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
-    paint: function() {
-        
+    paint: function(color) {
+        this.color = color
+
     }
 }
+
+let ferrari = {
+    make: "Ferrari",
+    model: "Dino",
+    color: "rosu",
+    year: 1978,
+    mileage: 525737,
+    maxSpeed: 180,
+    paint: function(color) {
+        this.color = color
+
+    }
+}
+dacia.paint("galben")
+console.log(dacia)
+console.log(ferrari)
+// in pagina web, la Inspect, Console, vedem toate aceste detalii
