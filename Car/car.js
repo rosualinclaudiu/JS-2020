f29662b1c4d90ae99cf9b77a2d5f6d53be3cc440
Alyn <rosualinclaudiu@yahoo.com>
2021-01-08 20:28:27 +0200

    1

--- Car/car.js
+++ Car/car.js
@@ -1,8 +1,11 @@
 let dacia = {
     make: "Dacia",
-    model: "1310",
+    model: 1310,
     color: "verde",
     year: 1978,
     mileage: 525737,
-    maxSpeed: 
+    maxSpeed: 180,
+    paint: function() {
+        
+    }
 }
