54cf0c6a2963fe744c3b8d1ec84871401b198a8b
Alyn <rosualinclaudiu@yahoo.com>
2021-01-11 20:38:22 +0200

    1

--- Planets/planets.js
+++ Planets/planets.js
@@ -1,7 +1,23 @@
-let planet = document.getElementById("redplanet")
+let button = document.getElementById("check")
+button.addEventListener("click", function () {
+    let planet = document.getElementById("redplanet")
 
-planet.innerText = "Alien Invasion"
-planet.classList.add("alert")
-//planet.style.color = "red" - astea 2 mutate in CSS
-//planet.style.fontWeight = "bold"
-//planet.remove() - sterge partea asta din element, in acest caz - redplanet
+    planet.innerText = "Alien Invasion"
+    planet.classList.add("alert")
+    // planet.style.color = "red" - astea 2 mutate in CSS
+    // planet.style.fontWeight = "bold"
+    // planet.remove() - sterge partea asta din element, in acest caz - redplanet
+})
+
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        let planet = document.getElementById("redplanet")
+
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+    })
+
+document.getElementById("greenplanet")
+    .addEventListener("click", function () {
+        document.getElementById("greenplanet").classList.add("alert")
+    })
