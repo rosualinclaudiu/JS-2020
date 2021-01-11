7da3017d81705af8ba2fc8fb33e9c1e8f2df517d
Alyn <rosualinclaudiu@yahoo.com>
2021-01-11 19:35:40 +0200

    1

---
+++ Planets/planets.js
@@ -0,0 +1,7 @@
+let planet = document.getElementById("redplanet")
+
+planet.innerText = "Alien Invasion"
+planet.classList.add("alert")
+//planet.style.color = "red" - astea 2 mutate in CSS
+//planet.style.fontWeight = "bold"
+//planet.remove() - sterge partea asta din element, in acest caz - redplanet
