d6132d45cacbd4fff50625676afefa5291b79c67
Alyn <rosualinclaudiu@yahoo.com>
2021-01-15 20:32:32 +0200

    1

--- Bubbles/bubbles.js
+++ Bubbles/bubbles.js
@@ -1,4 +1,4 @@
-var numberOfBubbles = 10
+var numberOfBubbles = 100
 for (let i = 0; i < numberOfBubbles; i++) {
     newBubble()
 }
@@ -6,7 +6,9 @@ function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
     let x = randomNumber(100)
+    let delay = randomNumber(4000)
     bubble.style.left = x + "vh"
+    bubble.style.animationDelay = delay + "ms"
     document.querySelector("body").appendChild(bubble)
 }
 function randomNumber(max) {
