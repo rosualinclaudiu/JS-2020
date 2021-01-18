03b4549de78e462c4569084ed7ede2124f210dcb
Alyn <rosualinclaudiu@yahoo.com>
2021-01-18 20:43:36 +0200

    1

--- Array/array.js
+++ Array/array.js
@@ -10,3 +10,16 @@ fruits.reverse()
 
 console.log(max)
 console.log(fruits)
+
+for (let index = 0; index < fruits.length; index++) {
+    const fruit = fruits[index];
+    listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+} // for loop
+
+for (const fruit of fruits) {
+    listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+} // for of
