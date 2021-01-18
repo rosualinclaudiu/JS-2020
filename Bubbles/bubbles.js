fe039a61459af2deacc9344be0d24c9fb71e41b0
Alyn <rosualinclaudiu@yahoo.com>
2021-01-18 20:09:21 +0200

    1

--- Bubbles/bubbles.js
+++ Bubbles/bubbles.js
@@ -1,4 +1,4 @@
-var numberOfBubbles = 100
+var numberOfBubbles = 40
 for (let i = 0; i < numberOfBubbles; i++) {
     newBubble()
 }
@@ -6,14 +6,24 @@ function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
     let x = randomNumber(100)
-    let delay = randomNumber(4000)
+    let delay = randomNumber(3000)
+    let red = randomNumber(255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(3) + 1
+
     // bubble.style.left = x + "vw"  (la fel ca cea de dedesubt)
     bubble.style.left = `${x}vw`
-    bubble.style.animationDelay = delay + "ms"
+    // bubble.style.animationDelay = delay + "ms"  (la fel ca cea de dedesubt)
+    bubble.style.animationDelay = `${delay}`
     // bubble.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")"    (la fel ca ea de dedesubt)
     bubble.style.borderColor = `rgb(${red},${green},${blue})`
+    bubble.style.height = bubble.style.width = `${size}em`
+    // bubble.style.height = `${size}em`
+    // bubble.style.width = `${size}em`
     document.querySelector("body").appendChild(bubble)
 }
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
+
--- Rectangle/rectangle.js
+++ Rectangle/rectangle.js
@@ -1,8 +1,6 @@
 document.querySelector('div')
-        .addEventListener("click", function() {
-            document.querySelector('div')
-                    .classList.add("disappear")
-        })
+        .addEventListener("click", () => document.querySelector('div').classList.add("disappear")
+        ) //  pentru o singura functie, poate fi scrisa in felul asta. Daca sunt mai multe, le scriem ca mai jos
 
         document.querySelector('div')
         .addEventListener("animationend", function() {
