c6a5146fcac16eec55f07cfda07757318aaa73df
Alyn <rosualinclaudiu@yahoo.com>
2021-01-18 18:06:44 +0200

    1

--- Bubbles/bubbles.js
+++ Bubbles/bubbles.js
@@ -7,8 +7,11 @@ function newBubble() {
     bubble.classList.add("bubble")
     let x = randomNumber(100)
     let delay = randomNumber(4000)
-    bubble.style.left = x + "vh"
+    // bubble.style.left = x + "vw"  (la fel ca cea de dedesubt)
+    bubble.style.left = `${x}vw`
     bubble.style.animationDelay = delay + "ms"
+    // bubble.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")"    (la fel ca ea de dedesubt)
+    bubble.style.borderColor = `rgb(${red},${green},${blue})`
     document.querySelector("body").appendChild(bubble)
 }
 function randomNumber(max) {
