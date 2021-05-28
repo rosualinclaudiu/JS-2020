740c941541cb71b8dd25359484232a4de439a81c
Alyn <rosualinclaudiu@yahoo.com>
2021-05-28 18:54:52 +0300

    1

--- Vue/Project/hanoi/src/components/Hanoi.vue
+++ Vue/Project/hanoi/src/components/Hanoi.vue
@@ -4,14 +4,21 @@
       v-for="tower in game.towers"
       :key="tower.id"
       class="tower"
+      @dragover.prevent
+      @drop="drop(tower.id, $event)"
+      :style="{
+          height: game.numberOfDisks + 'em'
+      }"
       >
       <div
         v-for="disk in tower.disks"
         :key="disk.size"
         class="disk"
+        draggable="true"
+        @dragstart="drag(tower.id, $event)"
         :style="{
             backgroundColor: disk.color.getCssColor(),
-            width: disk.size + 'em'
+            width: 1 + disk.size * (1 - (game.numberOfDisks - 1) * 0.01) + 'em'
         }"
       >
       </div>
@@ -23,11 +30,21 @@
 
 <script>
 
-import {Game} from '../classes/classes.js'
+import {Game, Move} from '../classes/classes.js'
 export default {
     data() {
         return {
-            game: new Game(5)
+            game: new Game(8)
+        }
+    },
+    methods: {
+        drag(towerId, event) {
+            event.dataTransfer.setData('from', towerId)
+        },
+        drop(towerId, event) {
+            let fromTowerId = event.dataTransfer.getData('from')
+            let move = new Move(fromTowerId, towerId)
+            this.game.make(move)
         }
     },
 }
@@ -40,6 +57,7 @@ export default {
     display: flex;
     justify-content: space-around;
     align-items: flex-end;
+    position: relative;
 }
 .tower {
     width: 30%;
@@ -47,17 +65,20 @@ export default {
     flex-direction: column-reverse;
     align-items: center;
     height: 50%;
+    position: relative;
 
 }
 .tower::before {
     content: '';
     background-color: brown;
-    height: 20%;
+    height: 110%;
     width: 0.5em;
     position: absolute;
     z-index: -1;
+    border-radius: 0.2em;
 }
 .disk {
     height: 1em;
+    border-radius: 0.2em;
 }
 </style>
