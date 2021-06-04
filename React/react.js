7f75ee2073096f62262b21dfedd219a26634f4fc
Alyn <rosualinclaudiu@yahoo.com>
2021-06-04 19:44:48 +0300

    1

--- React/react.js
+++ React/react.js
@@ -3,32 +3,34 @@ class Welcome extends React.Component {
         super(props)
     }
     render() {
-        return <h1>Hello {this.props.name}</h1> // { name.toUpperCase() } - pentru a scrie cu litere mari
+        return <h1>Hello {this.props.name } </h1> //this.props.name.toUpperCase() - pentru a scrie cu litere mari
     }
 }
 class Clicker extends React.Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         // starea initiala a componentei
         this.state = {
-            clicks: 0
+            clicks: 0,
         }
-    }
-    render() {
-        return <button onClick={this.Increment}>You clicked {this.state.clicks} times</button>
     }
     // definind metoda ca arrow function se face automat bind la this
     increment = () => {
         this.setState({
             clicks: this.state.clicks + 1
         })
+    };
+    render() {
+        return <button onClick={this.increment}>You clicked {this.state.clicks} times</button>
     }
 }
 
-const template = <div>
+const template = (
+<div>
     <Welcome name='Alyn'></Welcome>
     <Clicker></Clicker>
 </div>
+)
 
 ReactDOM.render(
     template,
