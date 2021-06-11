99c5ebecf81f623fc12653c5a37d9006bc857ea2
Alyn <rosualinclaudiu@yahoo.com>
2021-06-11 19:48:56 +0300

    1

--- 8. Back-End/RandomApi/index.js
+++ 8. Back-End/RandomApi/index.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 
 function random(min, max) {
     console.log(`min = ${min}, max = ${max}`)
-    return {
-        number: Math.random() * (max - min) + min
-        //number: Math.floor(Math.random() * (+max + 1 -+ min) + + min) // - este legata cu cea de mai jos
+    return { 
+        number: Math.floor(Math.random() * (max - min)) + min
     }
 }
 
-//  /random?min={x}$max={y}
+// /random?min={x}&max={y}
 app.get('/random', (req, resp) => { 
-    resp.send(random(parseInt(req.query.min), parseInt(req.query.max)))
-    //resp.send(random(req.query.min, req.query.max)) // - este legata cu cea de mai sus
+    resp.send(random(parseInt(req.query.min), parseInt(req.query.max))) 
 })
 
 app.listen(
     port,
-    () => { console.log('Listening on http://localhost:${port}') }
+    () => { console.log(`Listening on http://localhost:${port}`) }
 )
