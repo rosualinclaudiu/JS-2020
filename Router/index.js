cd5fdd6dffd5f5f8b50608ff171e33044d219099
Alyn <rosualinclaudiu@yahoo.com>
2021-06-11 19:07:03 +0300

    1

--- Router/index.js
+++ Router/index.js
@@ -2,51 +2,61 @@ import "https://unpkg.com/vue/dist/vue.js"
 import "https://unpkg.com/vue-router/dist/vue-router.js"
 
 const welcome = Vue.component(
-    'welcome', {
-        template: 
-        `<h1>Hello! Welcome to my website!</h1>`
+    'welcome', 
+    {
+        template: `<h1>Hello! Welcome to my website!</h1>`
     }
 )
 
 const login = Vue.component(
-    'login', {
+    'login',
+    {
         template: `
-        <form>
-            <label for="email">Email</label>
-            <input id="email" type="text">
-            <label for="password">Password</label>
-            <input id="password" type="text">
-            <input type="button" value="Sign in">
-        </form>
+            <form>
+                <label for="email">Email:</label>
+                <input id="email" type="text">
+                <label for="password">Password:</label>
+                <input id="password" type="text">
+                <input type="button" value="Sign in">
+            </form>
         `
     }
 )
 
 const signup = Vue.component(
-    'login', {
+    'signup',
+    {
         template: `
-        <form>
-            <label for="email">Email</label>
-            <input id="email" type="text">
-            <label for="password">Password</label>
-            <input id="password" type="text">
-            <label for="confirmPassword">Confirm Password</label>
-            <input id="confirmPassword" type="text">
-            <input type="button" value="Sign up">
-        </form>
+            <form>
+                <label for="email">Email:</label>
+                <input id="email" type="text">
+                <label for="password">Password:</label>
+                <input id="password" type="text">
+                <label for="confirmPassword">Confirm Password:</label>
+                <input id="confirmPassword" type="text">
+                <input type="button" value="Sign up">
+            </form>
         `
     }
 )
 
 const siteMenu = Vue.component(
-    'siteMenu', {
+    'siteMenu', 
+    {
         template: `
         <div>
             <router-link to="/">Home</router-link>
-            <router-link to="/">Sign Up</router-link>
-            <router-link to="/">Log In</router-link>
+            <router-link to="/signup">Sign Up</router-link>
+            <router-link to="/login">Log In</router-link>
         </div>
         `
+    }
+)
+
+const notFound = Vue.component(
+    'notFound',
+    {
+        template: `<img src="https://cdn.optinmonster.com/wp-content/uploads/2018/06/android-404-845x504.png">`
     }
 )
 
@@ -56,20 +66,19 @@ const myRouter = new VueRouter({
         { path: '/welcome', component: welcome },
         { path: '/login', component: login },
         { path: '/signup', component: signup },
+        { path: '*', component: notFound }
     ]
-    
- })
+})
 
 new Vue({
-    el:'#app',
+    el: '#app',
     template: `
-    <div>
-        <siteMenu></siteMenu>
-        <transition name="mytransition" mode="out-in">
-            <router-view></router-view>
-        </transition>
-        
-    </div>
+        <div>
+            <siteMenu></siteMenu>
+            <transition name="mytransition" mode="out-in">
+                <router-view></router-view>
+            </transition>
+        </div>
     `,
     router: myRouter
 })
